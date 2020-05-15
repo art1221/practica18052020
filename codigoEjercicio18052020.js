15058b7342c905c7dc4850bb9777bbe4ca7393f7
art1221 <arturo.rojas@utpuebla.edu.mx>
2020-05-15 14:12:33 -0500

    Add files via upload
    archivos fuentes de la práctica

---
+++ codigoEjercicio18052020.js
@@ -0,0 +1,21 @@
+var textoNumero = document.getElementById("txtNumero");
+var botonCalcular = document.getElementById("btnCalcular");
+var botonOtraTable = document.getElementById("btnReiniciar");
+
+botonCalcular.addEventListener("click",funcionTabla);
+botonOtraTable.addEventListener("click",funcionReiniciar);
+
+function funcionTabla(){
+  var numero = parseInt(textoNumero.value);
+
+  for (var i = 1; i <= 10; i++) {
+    var newdoc = document.createElement("div");
+    var t = document.createTextNode(numero + " X " + i + " = " + (numero*i));
+    newdoc.appendChild(t);
+    document.getElementsByTagName("h3")[0].appendChild(newdoc);
+  }
+}
+
+function funcionReiniciar(){
+  location.reload(true);
+}
